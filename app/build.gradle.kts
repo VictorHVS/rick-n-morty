171e98984bb41a1111ebe9754b9e0c7bc3548cc2
Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>
2023-07-30 00:22:55 -0300

    (chore) enabling compose metrics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,16 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+
+        if (project.findProperty("enableComposeCompilerReports") == "true") {
+            val outputDir = project.buildDir.path + "/compose-reports"
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=$outputDir",
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=$outputDir",
+            )
+        }
     }
     buildFeatures {
         compose = true
