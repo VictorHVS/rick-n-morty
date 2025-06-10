557e6c9404b3b1d74f3aeed69a6191ec71e84a6e
Victor Hugo Vieira de Sousa <victor@kinzoo.com>
2025-06-10 12:08:09 -0300

    feat: update app name, version, and SonarQube plugin
    This commit updates the application name to "RM Universe Explorer",
    increments the app version to 1.0.3 (versionCode 4), and upgrades
    the SonarQube Gradle plugin to version 4.4.1.3373.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.victorhvs.rnm"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
